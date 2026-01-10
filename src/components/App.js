3eb61e0eb048e5714fe1503a5110127ec866dfbf
HansaVermaGithub <hansaverma88@gmail.com>
2026-01-10 18:48:25 +0530

    Fix indentation in project description rendering

--- src/components/App.js
+++ src/components/App.js
@@ -19,7 +19,7 @@ const App = () => {
   {projects.map((project,index)=>(
     <div key={index}>
     <h1 data-ns-test="project-name">{project.name}</h1>
-  <h6 data-ns-test="project-description">{project.description}</h6>
+    <h6 data-ns-test="project-description">{project.description}</h6>
     </>
   )}
  
