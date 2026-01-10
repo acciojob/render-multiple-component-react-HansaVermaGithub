cbdde8a31798794c2ccbdccb337069659ad70b3b
HansaVermaGithub <hansaverma88@gmail.com>
2026-01-10 18:47:31 +0530

    Refactor App component to display projects
    Refactor App component to use local projects array and map through it for rendering project names and descriptions.

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,29 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
 
-const App = (data-ns-test) => {
+const App = () => {
+  const projects = [
+    {
+      name:"Project1",
+      description : "my first project"
+    },
+    {
+      name:"Project2",
+      description : "my second project"
+    }
+    
+  ];
   return (
     <div id="main" >
-    <h1>{data-ns-test.project-name}</h1>
-  <h2>{data-ns-test.project-description}</h2>
+    <div className="ns-wrapper">
+  {projects.map((project,index)=>(
+    <div key={index}>
+    <h1 data-ns-test="project-name">{project.name}</h1>
+  <h6 data-ns-test="project-description">{project.description}</h6>
+    </>
+  )}
+ 
+    </div>
     </div>
   )
 }
